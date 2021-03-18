260477b7aad9a8b46054b407406e4654675cf79a
jhonatanbeltran <jhonariston@gmail.com>
2021-03-17 23:52:37 -0400

    lock

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
- import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine, ChatFeed } from "react-chat-engine";
 
- import './App.css';
- 
+import ChatFeed from './components/ChatFeed';
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="32282cd8-dd44-4998-9d4e-66bb26673dba"
+            userName="chapa"
+            userSecret="chapa"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+};
+
